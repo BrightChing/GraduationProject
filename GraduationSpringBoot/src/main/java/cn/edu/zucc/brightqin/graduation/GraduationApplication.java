bea26f91e1d805b3b67a847a96b263b57deb7b5e
brightqin <bright.qin@qq.com>
2018-10-30 11:51:57 +0800

    2018-10-30 修改项目结构

---
+++ GraduationSpringBoot/src/main/java/cn/edu/zucc/brightqin/graduation/GraduationApplication.java
@@ -0,0 +1,15 @@
+package cn.edu.zucc.brightqin.graduation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author brightqin
+ */
+@SpringBootApplication
+public class GraduationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraduationApplication.class, args);
+    }
+}
